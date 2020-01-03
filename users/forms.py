8e9157e656f387ceaff7eb46ce9ad63724bde5ba
ezrankayamba <ezrankayamba@gmail.com>
2020-01-03 23:05:51 +0300

    Small fix

--- users/forms.py
+++ users/forms.py
@@ -14,7 +14,8 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserCreateForm(forms.ModelForm):
-    role = forms.ChoiceField(choices=map(lambda x: (x.id, x.name), Role.objects.all()))
+    # role = forms.ChoiceField(choices=map(lambda x: (x.id, x.name), Role.objects.all()))
+    role = forms.ChoiceField(choices=())
     username = forms.CharField(label="Username")
     email = forms.CharField(label="Email")
 
