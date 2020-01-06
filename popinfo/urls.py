f28d257d34e8ee26fbbf2b88328745be235d9c1e
ezrankayamba <ezrankayamba@gmail.com>
2020-01-06 15:33:59 +0300

    Url tzainfo fix

--- popinfo/urls.py
+++ popinfo/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='popinfo-home'),
     path('admin', views.admin, name='popinfo-admin'),
-    path('tnainfo', views.tna_info, name='popinfo-tnainfo'),
+    path('tzainfo', views.tna_info, name='popinfo-tnainfo'),
 ]
